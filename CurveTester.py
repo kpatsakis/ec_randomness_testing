9279c41385caac5bb2929be3b6a3afa7d14d1438
Costas Patsakis <kpatsak@gmail.com>
2020-02-07 19:33:10 +0200

    Added courves, cleaned code.

--- CurveTester.py
+++ CurveTester.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from random import randrange
 from fastecdsa import curve
-from fastecdsa.curve import P256, P521, P384
+from fastecdsa.curve import P192, P224, P256, P521, P384, secp192k1, secp224k1, secp256k1
 from fastecdsa.point import Point
 from ed25519 import *
 from multiprocessing import Pool
 from bitarray import bitarray
 import argparse
+
 
 def padnum(x, L):
     # gets a number, converts it to binary, pads it with zeros to fit a length and returns the *binary* format, which essentially is bitarray
@@ -30,20 +31,16 @@ def rnd_multpoint25519(UB):
 
 def TestCurve25519(TESTS):
     TESTS = FixTests(TESTS, 255)
-    prefix = "EC25519"
-    UB = 2**255 - 19
-    p = Pool(8)
-    lst = [UB] * TESTS
-    points = p.map(rnd_multpoint25519, lst)
+    prefix = "ED25519"
+    p = 2**255 - 19
+    pl = Pool(8)
+    lst = [p] * TESTS
+    points = pl.map(rnd_multpoint25519, lst)
     allbits = bitarray()
     for pnt in points:
         allbits += bitarray(padnum(pnt, 255))
     filesaver(allbits, 255, prefix)
     return True
-
-
-CurveP = {"P256": {"x": 0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296,
-                   "y": 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5, "UB": 2**256 - 2**224 + 2**192 + 2**96 - 1}, "P384": {"x": 0xaa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7, "UB": 2**384 - 2**128 - 2**96 + 2**32 - 1, "y": 0x3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f}, "P521": {"UB": 2**521 - 1, "x": 0xc6858e06b70404e9cd9e3ecb662395b4429c648139053fb521f828af606b4d3dbaa14b5e77efe75928fe1dc127a2ffa8de3348b3c1856a429bf97e7e31c2e5bd66, "y": 0x11839296a789a3bc0045c8a5fb42c7d1bd998f54449579b446817afbd17273e662c97ee72995ef42640c550b9013fad0761353c7086a272c24088be94769fd16650}}
 
 
 def rnd_multpoint(l):
@@ -55,22 +52,33 @@ def rnd_multpoint(l):
 
 def test_curve(c, TESTS):
     if c == "Curve25519":
+        TESTS = FixTests(TESTS, 255)
         TestCurve25519(TESTS)
         return True
     else:
-        if c == "P256":
+        if c == "P192":
+            E = curve.P192
+        elif c == "P224":
+            E = curve.P224
+        elif c == "P256":
             E = curve.P256
         elif c == "P384":
             E = curve.P384
         elif c == "P521":
             E = curve.P521
-        L = int(c[1:])
-        TESTS = FixTests(TESTS, L)
-    x, y, UB = E.gx, E.gy, E.p
+        elif c == "secp192k1":
+            E = curve.secp192k1
+        elif c == "secp224k1":
+            E = curve.secp224k1
+        elif c == "secp256k1":
+            E = curve.secp256k1
+    x, y, p = E.gx, E.gy, E.p
+    L = len(bin(p)) - 2
+    TESTS = FixTests(TESTS, L)
     P = Point(x, y, E)
-    p = Pool(8)
-    lst = [[UB, P]] * TESTS
-    points = p.map(rnd_multpoint, lst)
+    pl = Pool(8)
+    lst = [[q, P]] * TESTS
+    points = pl.map(rnd_multpoint, lst)
     allbits = bitarray()
     for pnt in points:
         pnt = padnum(pnt, L)
@@ -117,11 +125,13 @@ def filesaver(barray, L, prefix):
     del q4
     return True
 
-parser = argparse.ArgumentParser(prog='CurveTester', description='Collects statistics by computing random points on elliptic curves Ed25519, P256, P384, and P521. All statistics are saved in raw files, overwriting existing ones. If you ask for for many tests, this would be a slow process, so better grab a coffee.')
-parser.add_argument('-t','--tests', type=int, default=1000,
-                   help='Minimum tests to perform, default set to 1000.')
+
+parser = argparse.ArgumentParser(
+    prog='CurveTester', description='Collects statistics by computing random points on elliptic curves Ed25519, P256, P384, and P521. All statistics are saved in raw files, overwriting existing ones. If you ask for for many tests, this would be a slow process, so better grab a coffee.')
+parser.add_argument('-t', '--tests', type=int, default=10000,
+                    help='Minimum tests to perform, default set to 1000.')
 
 args = parser.parse_args()
-for ec in ["Curve25519", "P256", "P384", "P521"]:
+for ec in ["Curve25519", "P192", "P224", "P256", "P384", "P521", "secp256k1", "secp192k1", "secp224k1"]:
     print ("Working with curve: %s..." % ec)
     test_curve(ec, args.tests)
